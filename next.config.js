474d6cd99cb1bb7b23ce44e8f487d900317f4030
Usman Ghani <usman_ganik@hotmail.com>
2026-03-08 18:58:03 +0400

    test

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  serverActions: {
+    // IMPORTANT: domains only — no https://
+    allowedOrigins: [
+      "capstone-pg.onrender.com",
+      "demo.uae-prep.app.jaggaer.com",
+      // add any other customer portal domains here
+    ],
+  },
+};
 
 module.exports = nextConfig;
